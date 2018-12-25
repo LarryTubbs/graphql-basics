a76df4decb44f5c0f16dd48a003d6b56abf92863
LarryTubbs <larry.tubbs@gmail.com>
2018-12-25 11:31:19 -0600

    added delete operations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import {GraphQLServer} from "graphql-yoga";
+import uuidv4 from "uuid/v4";
 
 // demo data
-const users = [{
+let users = [{
     id: '1',
     name: 'Larry',
     email: 'larry@example.com',
@@ -23,7 +24,7 @@ const users = [{
     age: 12
 }];
 
-const posts = [{
+let posts = [{
     id: '11',
     title: 'GraphQL is pretty cool',
     body: 'This could be the future of API development.  We\'ll see.',
@@ -43,23 +44,23 @@ const posts = [{
     author: '2'
 }];
 
-const comments = [{
-    id: 101,
+let comments = [{
+    id: "101",
     text: 'I think it just might be.',
     author: '1',
     post: '11'
 }, {
-    id: 102,
+    id: "102",
     text: 'This could really help us on core services.',
     author: '1',
     post: '11'
 }, {
-    id: 103,
+    id: "103",
     text: 'Maybe not.  The resolvers seem to be written in such a way as to maximize performance.',
     author: '2',
     post: '12'
 }, {
-    id: 104,
+    id: "104",
     text: 'This is fun',
     author: '3',
     post: '12'
@@ -73,6 +74,34 @@ const typeDefs = `
         post: Post!
         posts(query: String): [Post!]!
         comments: [Comment!]!
+    }
+
+    type Mutation {
+        createUser(data: CreateUserInput!): User!
+        deleteUser(id: ID!): User!
+        createPost(data: CreatePostInput!): Post!
+        deletePost(id: ID!): Post!
+        createComment(data: CreateCommentInput): Comment!
+        deleteComment(id: ID!): Comment!
+    }
+
+    input CreateUserInput {
+        name: String!,
+        email: String!,
+        age: Int
+    }
+
+    input CreatePostInput {
+        title: String!, 
+        body: String!, 
+        published: Boolean!, 
+        author: ID!
+    }
+
+    input CreateCommentInput {
+        text: String!, 
+        author: ID!, 
+        post: ID!
     }
 
     type User {
@@ -181,6 +210,119 @@ const resolvers = {
             return posts.find((post) => {
                 return post.id === parent.post;
             });
+        }
+    },
+    Mutation: {
+        createUser (parent, args, ctx, info) {
+            // ensure user isn't already in the array by email
+            const emailTaken = users.some((user) => {
+                return user.email === args.data.email;
+            });
+            if (emailTaken) {
+                throw new Error(`Already have a user with the email ${args.data.email}`);
+            };
+            const user = {
+                id: uuidv4(),
+                ...args.data
+            };
+            users.push(user);
+            return user;
+        },
+        deleteUser (parent, args, ctx, info) {
+            // if user found
+            const userIndex = users.findIndex((user) => {
+                return user.id === args.id;
+            });
+            if (userIndex === -1) {
+                throw new Error('User not found.');
+            }
+            // delete all comments
+            comments = comments.filter((comment) => {
+                return comment.author !== args.id;
+            });
+            // delete all posts
+            posts = posts.filter((post) => {
+                const match = post.author === args.id
+                // if match, delete its comments
+                if (match) {
+                    comments = comments.filter((comment) => {
+                        return comment.post !== post.id;
+                    });
+                };
+                return !match; // keep only non-matched posts
+            });
+            // delete the user
+            const deletedUsers = users.splice(userIndex, 1);
+            return deletedUsers[0];
+        },
+        createPost (parent, args, ctx, info) {
+            // make sure the author id matches an existing user
+            const userExists = users.some((user) => {
+                return user.id === args.data.author;
+            });
+            if (!userExists) {
+                throw new Error(`No user exists with id: ${args.data.author}.`);
+            };
+            const post = {
+                id: uuidv4(),
+                ...args.data
+            };
+            posts.push(post);
+            return post;
+        },
+        deletePost (parent, args, ctx, info) {
+            // does post exist, if not throw error
+            const postIndex = posts.findIndex ((post) => {
+                return post.id === args.id;
+            });
+            if (postIndex === -1) {
+                throw new Error('Post not found.');
+            };
+
+            // remove the post
+            const removedPosts = posts.splice(postIndex, 1);
+
+            // remove all comments associated with the post
+            comments = comments.filter((comment) => {
+                return comment.post !== args.id;
+            });
+
+            // return the removed post
+            return removedPosts[0];
+        },
+        createComment (parent, args, ctx, info) {
+            const userExists = users.some((user) => {
+                return user.id === args.data.author;
+            });
+            if (!userExists) {
+                throw new Error(`No user exists with id: ${args.data.author}.`);
+            };
+            const publishedPostExists = posts.some((post) => {
+                return post.id === args.data.post && post.published;
+            });
+            if (!publishedPostExists) {
+                throw new Error(`No published post exists with id: ${args.data.post}.`);
+            };
+            const comment = {
+                id: uuidv4(),
+                ...args.data
+            };
+            comments.push(comment);
+            return comment;
+        },
+        deleteComment (parent, args, ctx, info) {
+            // does the comment exist, if not throw error
+            const commentIndex = comments.findIndex((comment) => {
+                return comment.id === args.id;
+            });
+
+            if (commentIndex === -1) {
+                throw new Error('No comment found.');
+            };
+
+            // remove the comment and return it
+            const foundComments = comments.splice(commentIndex, 1);
+            return foundComments[0];
         }
     }
 };
